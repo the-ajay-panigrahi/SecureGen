247dc1b448497fef796b2c91efaf6fcf5e31998a
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-27 17:53:57 +0530

    password generator logic

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,30 @@ function App() {
   const [numberCheck, setNumberCheck] = useState(false);
   const [charCheck, setCharCheck] = useState(false);
 
-  const passwordGenerator = useCallback(() => {}, []);
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let passString = "";
+    if (upperCaseCheck) passString += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (lowerCaseCheck) passString += "abcdefghijklmnopqrstuvwxyz";
+    if (numberCheck) passString += "0123456789";
+    if (charCheck) passString += "!@#$%^&*-_+=[]{}~`";
 
-  useEffect(passwordGenerator(), []);
+    for (let i = 1; i <= length; i++) {
+      let index = Math.floor(Math.random() * passString.length + 1);
+      pass += passString[index];
+    }
+
+    setPassword(pass);
+  }, [
+    upperCaseCheck,
+    lowerCaseCheck,
+    numberCheck,
+    charCheck,
+    length,
+    setPassword,
+  ]);
+
+  // useEffect(passwordGenerator(), []);
 
   return (
     <>
@@ -35,7 +56,9 @@ function App() {
           <div className="rounded-xl bg-white p-5 flex flex-col gap-3">
             <div className="flex justify-between items-center gap-8">
               <div className="text-xl font-extrabold">Password Length</div>
-              <div class=" text-xl font-extrabold text-red-500">{length}</div>
+              <div className=" text-xl font-extrabold text-red-500">
+                {length}
+              </div>
             </div>
             <div>
               <input
@@ -46,73 +69,73 @@ function App() {
                 onChange={(event) => {
                   setlength(event.target.value);
                 }}
-                class="w-full h-2 rounded-lg appearance-none cursor-pointer bg-slate-800"
+                className="w-full h-2 rounded-lg appearance-none cursor-pointer bg-slate-800"
               />
             </div>
-            <div class="flex items-center">
+            <div className="flex items-center">
               <input
                 id="upperCheckBox"
                 type="checkbox"
                 value=""
-                class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+                className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 onChange={() => {
                   setUpperCaseCheck((prev) => !prev);
                 }}
               />
               <label
                 htmlFor="upperCheckBox"
-                class="ms-2 text-lg font-medium text-black"
+                className="ms-2 text-lg font-medium text-black"
               >
                 Includes Uppercase Letters
               </label>
             </div>
-            <div class="flex items-center">
+            <div className="flex items-center">
               <input
                 id="lowerCheckBox"
                 type="checkbox"
                 value=""
-                class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+                className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 onChange={() => {
                   setLowerCaseCheck((prev) => !prev);
                 }}
               />
               <label
                 htmlFor="lowerCheckBox"
-                class="ms-2 text-lg font-medium text-black"
+                className="ms-2 text-lg font-medium text-black"
               >
                 Includes Lowercase Letters
               </label>
             </div>
-            <div class="flex items-center">
+            <div className="flex items-center">
               <input
                 id="numberCheckBox"
                 type="checkbox"
                 value=""
-                class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+                className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 onChange={() => {
                   setNumberCheck((prev) => !prev);
                 }}
               />
               <label
                 htmlFor="numberCheckBox"
-                class="ms-2 text-lg font-medium text-black"
+                className="ms-2 text-lg font-medium text-black"
               >
                 Includes Numbers
               </label>
             </div>
-            <div class="flex items-center">
+            <div className="flex items-center">
               <input
                 id="charCheckBox"
                 type="checkbox"
                 value=""
-                class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+                className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 onChange={() => {
                   setCharCheck((prev) => !prev);
                 }}
               />
               <label
                 htmlFor="charCheckBox"
-                class="ms-2 text-lg font-medium text-black"
+                className="ms-2 text-lg font-medium text-black"
               >
                 Includes Symbols
               </label>
